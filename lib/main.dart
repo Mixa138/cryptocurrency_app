48e3cd2a4e4c0123ea2d784921ba229dd34baf8d
user <ti-mm@mail.ru>
2023-06-07 20:59:21 +0800

    Initial commit add

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
+import 'package:flutter_svg/svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,29 +15,44 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: '',
       theme: ThemeData(
-        scaffoldBackgroundColor: Colors.black,
+        dividerColor: Colors.white10,
+        scaffoldBackgroundColor: Color.fromARGB(255, 31, 31, 31),
         primarySwatch: Colors.grey,
+        appBarTheme: const AppBarTheme(
+            backgroundColor: Color.fromARGB(255, 31, 31, 31),
+            titleTextStyle: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            )),
+        listTileTheme: const ListTileThemeData(iconColor: Colors.white),
         textTheme: TextTheme(
-          bodyMedium: const TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 20,),
-          labelSmall: TextStyle(color: Colors.white.withOpacity(0.6), fontWeight: FontWeight.w700, fontSize: 14),
+          bodyMedium: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w500,
+            fontSize: 20,
+          ),
+          labelSmall: TextStyle(
+              color: Colors.white.withOpacity(0.6),
+              fontWeight: FontWeight.w700,
+              fontSize: 14),
         ),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: ''),
+      home: const CryptoListScreen(title: ''),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class CryptoListScreen extends StatefulWidget {
+  const CryptoListScreen({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<CryptoListScreen> createState() => _CryptoListScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _CryptoListScreenState extends State<CryptoListScreen> {
   int _counter = 0;
 
   void _incrementCounter() {
@@ -46,26 +61,56 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+
+   
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.yellow,
-        title: Text(widget.title),
+        title: Center(child: const Text('CryptoCurrenciesList')),
       ),
-      body: ListView.builder(
+      body: ListView.separated(
         itemCount: 10,
+        separatorBuilder: (context, index) => Divider(),
         itemBuilder: (context, i) => ListTile(
-          title: Text('Bitcoin', style: theme.textTheme.bodyMedium,),
-        subtitle: Text('2000\$', style: theme.textTheme.labelSmall,),
+          leading: SvgPicture.asset(
+            'assets/svg/bitcoin.svg',
+            width: 25,
+            height: 25,
+          ),
+          title: Text(
+            'Bitcoin',
+            style: theme.textTheme.bodyMedium,
+          ),
+          subtitle: Text(
+            '2000\$',
+            style: theme.textTheme.labelSmall,
+          ),
+          trailing: Icon(
+            Icons.arrow_forward_ios,
+            color: Colors.white,
+          ),
+          onTap: ()
+          {
+            Navigator.of(context).push(MaterialPageRoute(builder: (context)=> CryptoCoinScreen()));
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class CryptoCoinScreen extends StatelessWidget {
+  const CryptoCoinScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Center(child: Text('Crypto Coin')),
+      ),
     );
   }
 }
